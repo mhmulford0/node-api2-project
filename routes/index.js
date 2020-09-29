6ee31b701a89bdd1136008ba68bcd5d8fb11ccae
Matthew Mulford <mhmulford0@gmail.com>
2020-09-29 17:44:27 -0500

    refactor code

--- routes/index.js
+++ routes/index.js
@@ -4,35 +4,45 @@ const router = express.Router();
 const db = require("../data/db");
 
 router.get("/", async (req, res) => {
-  const result = await db.find();
-  result
-    ? res.status(200).json(result)
-    : res
-        .status(500)
-        .json({ error: "The posts information could not be retrieved." });
+  try {
+    const result = await db.find();
+    res.status(200).json(result);;
+  } catch (error) {
+    res.status(500).json({  error: `${error}`  });;
+  }
+  
 });
 
 router.get("/:id", async (req, res) => {
   const { id } = req.params;
-
-  const result = await db.findById(id);
-  console.log(result);
-  result.length > 0
+  
+  try {
+    const result = await db.findById(id);
+    result.length > 0
     ? res.status(200).json(result)
     : res
         .status(500)
         .json({ message: "The post with the specified ID does not exist." });
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ message: "The post with the specified ID does not exist."  });;
+  }
 });
 
 router.get("/:id/comments", async (req, res) => {
   const { id } = req.params;
-  const result = await db.findCommentById(id);
-
-  result.length > 0
+  
+  try {
+    const result = await db.findCommentById(id);
+    result.length > 0
     ? res.status(200).json(result)
     : res
         .status(500)
         .json({ message: "The post with the specified ID does not exist." });
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ message: "The post with the specified ID does not exist."  });;
+  }
 });
 
 module.exports = router;
