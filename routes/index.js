8460dfa35dd745caf7fd5e46d29d69019d0c8336
Matthew Mulford <mhmulford0@gmail.com>
2020-09-29 15:27:48 -0500

    get post by id

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,23 @@ const db = require("../data/db");
 
 router.get("/", async (req, res) => {
   const result = await db.find();
+  result
+    ? res.status(200).json(result)
+    : res
+        .status(500)
+        .json({ error: "The posts information could not be retrieved." });
+});
+
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  const result = await db.findById(id);
   console.log(result);
-  res.status(200).json(result);
+  result.length > 0
+    ? res.status(200).json(result)
+    : res
+        .status(500)
+        .json({ message: "The post with the specified ID does not exist." });
 });
 
 module.exports = router;
